77a2a4747da786d07ad97c9ebd18d141c445412d
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-04-28 07:28:50 -0400

    Se crearon los casos de uso para las películas populares, mejor calificadas y proximos estrenos

--- src/presentation/hooks/useMovies.tsx
+++ src/presentation/hooks/useMovies.tsx
@@ -1,6 +1,5 @@
 import {useEffect, useState} from 'react';
 import type {Movie} from '../../core/entities/movie.entity';
-
 import * as UseCases from '../../core/use-cases';
 import {movieDBFetcher} from '../../config/adapters/movieDB.adapter';
 
@@ -9,6 +8,9 @@ export const useMovies = () => {
   const [isLoading, setIsLoading] = useState(true);
 
   const [nowPlaying, setNowPlaying] = useState<Movie[]>([]);
+  const [popular, setPopular] = useState<Movie[]>([]);
+  const [topRated, setTopRated] = useState<Movie[]>([]);
+  const [upcoming, setUpcoming] = useState<Movie[]>([]);
 
   useEffect(() => {
     initialLoad();
@@ -16,7 +18,27 @@ export const useMovies = () => {
 
   const initialLoad = async () => {
 
-    const nowPlayingMovies = await UseCases.moviesNowPlayingUseCase(movieDBFetcher);
+    const nowPlayingPromise = UseCases.moviesNowPlayingUseCase(movieDBFetcher);
+    const popularPromise = UseCases.moviesPopularUseCase(movieDBFetcher);
+    const topRatedPromise = UseCases.moviesTopRatedUseCase(movieDBFetcher);
+    const upcomingPromise = UseCases.moviesUpcomingUseCase(movieDBFetcher);
+
+    const [
+      nowPlayingMovies,
+      popularMovies,
+      topRatedMovies,
+      upcomingMovies,
+    ] = await Promise.all([
+      nowPlayingPromise,
+      popularPromise,
+      topRatedPromise,
+      upcomingPromise,
+    ]);
+
+    setNowPlaying( nowPlayingMovies );
+    setPopular( popularMovies );
+    setTopRated( topRatedMovies );
+    setUpcoming( upcomingMovies );
 
     setIsLoading(false);
 
@@ -25,5 +47,8 @@ export const useMovies = () => {
   return {
     isLoading,
     nowPlaying,
+    popular,
+    topRated,
+    upcoming,
   };
 };

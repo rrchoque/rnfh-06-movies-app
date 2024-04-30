d2eaca7bf27b9b00f42e153e1b4e29365b0f83aa
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-04-30 10:57:45 -0400

    Se habilitaron las variables de entorno

--- src/config/adapters/movieDB.adapter.ts
+++ src/config/adapters/movieDB.adapter.ts
@@ -1,10 +1,11 @@
+import { THE_MOVIE_DB_KEY } from '@env';
 import { AxiosAdapter } from './http/axios.adapter';
 
 
 export const movieDBFetcher = new AxiosAdapter({
   baseUrl: 'https://api.themoviedb.org/3/movie',
   params: {
-    api_key: '7c5b3a50982e27ac46aac3ac08a64e4f',
+    api_key: THE_MOVIE_DB_KEY ?? 'no-key',
     language: 'es'
   }
 });
